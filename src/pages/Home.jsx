0a75de00bb614c13dacbbc7aef065e5fc503ffd1
Van Danny Hoang <vhoang@hm.edu>
2025-04-26 22:34:36 +0200

    ADD team and reference section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
 import Hero from "../components/Hero";
-import Navbar from "../components/NavBar"
+import Navbar from "../components/NavBar";
 import About from "../components/About";
+import References from "../components/References";
+import Team from "../components/Team";
 // import Services from "../components/Services";
 // import Contact from "../components/Contact";
 // import Footer from "../components/Footer";
@@ -11,6 +13,9 @@ export default function Home() {
       <Navbar/>
       <Hero/>
       <About/>
+      <Team/>
+      <References/>
+      
       {/* <Hero />
       <About />
       <Services />
